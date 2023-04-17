2ea5286fbd8d95912989a469441887f3d1c23c8f
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-04-18 01:46:53 +0300

    Updates

--- src/components/House.js
+++ src/components/House.js
@@ -5,10 +5,20 @@ import { BiBed, BiBath, BiArea} from 'react-icons/bi';
 const House = ({House}) => {
   const { image, type, county, address, bedrooms, bathrooms, surface, price} = house;
   return (
+    <div className='bg-white shadow-1 p-5 rounded-lg rounded-tl-[90px] w-full max-w-[352px]
+    mx-auto cursor-pointer hover:shadow-2xl transition ' >
+      <image className='mb-8' src={image} alt=''/>
+      <div className='mb-4 flex gap-x-2 text-sm'>
+      <div className='bg-green-500 rounded-full text-white px-3'>{type}</div>
+      <div className='bg-violet-500 rounded-full text-white px-3'>{county}</div>
+    </div>
+    <div className='text-lg font-semibold max-w-[260px]'>{address}</div>
     <div>
-      <image src={image} alt=''/>
-      <div className='flex justify-between items-center'>
-        </div>
+      <div>
+        <BiBed />
+      </div>
+    </div>
+    <div className='text-lg font-semibold text-violet-600 mb-4'>{price}</div>
     </div>
   );
 };
