49036319e246827885a65cf9385948bdcb0338e2
Ndegwacodes_ <50831913+NdegwaCodes@users.noreply.github.com>
2023-04-18 12:20:08 +0300

    Updates

--- src/components/House.js
+++ src/components/House.js
@@ -13,12 +13,27 @@ const House = ({House}) => {
       <div className='bg-violet-500 rounded-full text-white px-3'>{county}</div>
     </div>
     <div className='text-lg font-semibold max-w-[260px]'>{address}</div>
-    <div>
-      <div>
+    <div className='flex gap-x-4 my-4'>
+    <div className='flex items-center text-gray-600 gap-1'>
+      <div className='text-[20px]'>
         <BiBed />
       </div>
+      <div>{bedrooms}</div>
+    </div>
+    <div className='flex items-center text-gray-600 gap-1'>
+      <div className='text-[20px]'>
+        <BiBath />
+      </div>
+      <div>{bathrooms}</div>
+    </div>
+    <div className='flex items-center text-gray-600 gap-1'>
+      <div className='text-[20px]'>
+        <BiArea />
+      </div>
+      <div>{surface}</div>
     </div>
     <div className='text-lg font-semibold text-violet-600 mb-4'>{price}</div>
+    </div>
     </div>
   );
 };
--- src/components/HouseList.js
+++ src/components/HouseList.js
@@ -11,7 +11,7 @@ const HouseList = () => {
   const { houses, loading} = useContext(HouseContext);
   return <section className='mb-20'>
     <div className="container mx-auto">
-      <div>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-14'>
         {houses.map((house,index) => {
           return (
             <Link to={`/property/${house.id}`} key={index}>
